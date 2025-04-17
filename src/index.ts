f18930af5ee894846b4efe07a943a7bf5272d51a
ubuntu <rishabhsethi29@gmail.com>
2025-04-17 23:07:31 +0530

    fix(example): Removed example from index

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,3 @@ export * from './logger/layouts/Layout';
 export * from './logger/layouts/PatternLayout';
 export * from './logger/appenders/Appender';
 export * from './logger/appenders/ConsoleAppender';
-
-// Example usage
-import { LogManager } from './logger/core/LogManager';
-import { ConsoleAppender } from './logger/appenders/ConsoleAppender';
-
-// Get a logger instance
-const logger = LogManager.getLogger('MyApp');
-
-// Add console appender
-logger.addAppender(new ConsoleAppender());
-
-// Log some messages
-logger.info('Application started');
-logger.debug('Debug message with parameter: {}', 42);
-try {
-    throw new Error('Something went wrong');
-} catch (error) {
-    logger.error('An error occurred', error);
-}
